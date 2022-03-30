d9339b8c93ea5e0719b77a165cde9b38dca7d33c
henderiw <wim.henderickx@gmail.com>
2022-03-30 07:57:03 +0200

    add nodeitfceselctor

--- pkg/abstract/abstract.go
+++ pkg/abstract/abstract.go
@@ -1,5 +1,14 @@
 package abstract
 
+import (
+	"context"
+
+	"github.com/yndd/ndda-network/pkg/nodeitfceselector"
+	nddov1 "github.com/yndd/nddo-runtime/apis/common/v1"
+	"github.com/yndd/nddo-runtime/pkg/resource"
+)
+
 type Abstract interface {
 	GetInterfaceName(itfcName string) (string, error)
+	GetSelectedNodeItfces(ctx context.Context, mg resource.Managed, epgSelectors []*nddov1.EpgInfo, nodeItfceSelectors map[string]*nddov1.ItfceInfo) (nodeitfceselector.SelectedNodes, error)
 }
---
+++ pkg/nodeitfceselector/nodeitfceselector.go
@@ -0,0 +1,13 @@
+package nodeitfceselector
+
+import "github.com/yndd/ndda-network/pkg/ndda/itfceinfo"
+
+type SelectedNodes struct {
+	Nodes map[string]SelectedInterfaces
+}
+
+type SelectedInterfaces struct {
+	Interfaces map[string]itfceinfo.ItfceInfo
+}
+
+
